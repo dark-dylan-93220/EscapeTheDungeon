71527dddd5e867ed68b318b356b35edb090eec95
dhollemaert <dhollemaert@GCFRPA046>
2025-01-17 14:28:00 +0100

    Ajoutez des fichiers projet.

---
+++ EscapeTheDungeon/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main() {
+	std::vector<sf::VideoMode> fullscreenModes = sf::VideoMode::getFullscreenModes();
+	sf::RenderWindow window(fullscreenModes.at(0), "Escape The Dungeon", sf::Style::Fullscreen);
+
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(50, 50));
+	rectangle.setFillColor(sf::Color::Blue
+	rectangle.setPosition(sf::Vector2f(0, 0));
+
+	sf::Event event;
+
+	while (window.isOpen()) {
+		// Poll events
+
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				std::cout << "Ferme" << std::endl;
+				break;
+			}
+		}
+
+		// Draw / Render
+		window.clear();
+		window.draw(rectangle);
+		window.display();
+	}
+
+	std::cout << "Hello" << std::endl;
+}
